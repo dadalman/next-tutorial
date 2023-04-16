bcadc8ade22583d08fd0227dd2f5e93030da0f15
dadalman <dadalmanofficial@gmail.com>
2023-04-17 02:19:22 +0800

    Progressively rendering a page

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
 
 type Params = {
   params: {
@@ -13,13 +14,17 @@ export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
   const userPostsData: Promise<Post[]> = getUserPosts(userId);
 
-  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  //const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+  const user = await userData;
 
   return (
     <>
       <h2>{user.name}</h2>
       <br />
-      <UserPosts posts={userPosts} />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts posts={userPosts} />
+      </Suspense>
     </>
   );
 }
